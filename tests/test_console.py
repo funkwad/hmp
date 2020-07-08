524e5aa6800296536eecfc68a8dfc3135538e429
Robert Beers <github-funkwad-signup@eers.org>
2020-07-08 11:26:37 -0700

    Add docstrings for console test cases.

--- tests/test_console.py
+++ tests/test_console.py
@@ -25,21 +25,32 @@ def mock_wikipedia_random_page(mocker: MockFixture) -> Mock:
 
 
 def test_main_succeeds(runner: CliRunner, mock_requests_get: Mock) -> None:
+    """It exits with a status code of 0."""
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
+
+
+@pytest.mark.e2e
+def test_main_succeeds_in_production_env(runner: CliRunner) -> None:
+    """It exits with a status code of 0 (end-to-end)."""
     result = runner.invoke(console.main)
     assert result.exit_code == 0
 
 
 def test_main_prints_title(runner: CliRunner, mock_requests_get: Mock) -> None:
+    """It prints out the title of the Wikipedia page."""
     result = runner.invoke(console.main)
     assert "Lorem Ipsum" in result.output
 
 
 def test_main_invokes_request_get(runner: CliRunner, mock_requests_get: Mock) -> None:
+    """It invokes request.get()."""
     runner.invoke(console.main)
     assert mock_requests_get.called
 
 
 def test_main_uses_en_wikipedia_org(runner: CliRunner, mock_requests_get: Mock) -> None:
+    """It uses the English Wikipedia by default."""
     runner.invoke(console.main)
     args, _ = mock_requests_get.call_args
     assert "en.wikipedia.org" in args[0]
@@ -48,6 +59,7 @@ def test_main_uses_en_wikipedia_org(runner: CliRunner, mock_requests_get: Mock) 
 def test_main_fails_on_request_error(
     runner: CliRunner, mock_requests_get: Mock
 ) -> None:
+    """It exists with a non-zero status code if the request fails."""
     mock_requests_get.side_effect = Exception("Boom")
     result = runner.invoke(console.main)
     assert result.exit_code == 1
@@ -56,6 +68,7 @@ def test_main_fails_on_request_error(
 def test_main_prints_message_on_request_error(
     runner: CliRunner, mock_requests_get: Mock
 ) -> None:
+    """It prints an error message if the request fails."""
     mock_requests_get.side_effect = requests.RequestException
     result = runner.invoke(console.main)
     assert "Error" in result.output
@@ -64,11 +77,6 @@ def test_main_prints_message_on_request_error(
 def test_main_uses_specified_language(
     runner: CliRunner, mock_wikipedia_random_page: Mock
 ) -> None:
+    """It uses the Wikipedia edition of the specified language."""
     runner.invoke(console.main, ["--language=pl"])
     mock_wikipedia_random_page.assert_called_with(language="pl")
-
-
-@pytest.mark.e2e
-def test_main_succeeds_in_production_env(runner: CliRunner) -> None:
-    result = runner.invoke(console.main)
-    assert result.exit_code == 0
