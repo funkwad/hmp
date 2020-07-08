960acc42a6c403a3452e17e2516dc4083d93631d
Robert Beers <github-funkwad-signup@eers.org>
2020-07-08 11:30:22 -0700

    Add docstrings for wikipedia test cases.

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -10,25 +10,27 @@ from hmp import wikipedia
 
 
 def test_random_page_uses_given_language(mock_requests_get: Mock) -> None:
+    """It selects the Wikipedia edition of the specified language."""
     wikipedia.random_page(language="de")
     args, _ = mock_requests_get.call_args
     assert "de.wikipedia.org" in args[0]
 
 
 def test_random_page_returns_page(mock_requests_get: Mock) -> None:
+    """It returns an object of type Page."""
     page = wikipedia.random_page()
     assert isinstance(page, wikipedia.Page)
 
 
 def test_random_page_handles_validation_errors(mock_requests_get: Mock) -> None:
+    """It raises 'ClickException' when validation fails."""
     mock_requests_get.return_value.__enter__.return_value.json.return_value = None
     with pytest.raises(click.ClickException):
         wikipedia.random_page()
 
 
 def test_trigger_typeguard(mock_requests_get: Mock) -> None:
-    """Cause a typeguard TypeError yet pass pytest dynamic testing.
-    Specifically, we pass an int as the language rather than a string."""
+    """It triggers a Typeguard TypeError when invalid language type used."""
     import json
 
     data = json.loads('{ "language": 1 }')
